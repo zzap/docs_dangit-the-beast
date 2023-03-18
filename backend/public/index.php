9dfed4c87e04652e5d2812ec8788d6738e47d1a6
Andreas Heigl <andreas@heigl.org>
2023-03-18 17:26:33 +0100

    Add editorconfig

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,8 +2,8 @@
 
 use Docsdangit\Backend\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+	return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -9,5 +9,6 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+	use MicroKernelTrait;
 }
+
