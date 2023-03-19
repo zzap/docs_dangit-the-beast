00cb939c434204896f327232017c95a154609314
Sven Wagener <sven@awesome.ug>
2023-03-19 12:40:55 +0100

    Got WordPress Docs test running

--- tests/WordPress_Docs_Test.php
+++ tests/WordPress_Docs_Test.php
@@ -12,59 +12,52 @@ class WordPress_Docs_Test extends Docs_Dangit_Test {
     public function test_parser() {
         $parser = new WordPress_Docs();
 
-        $item = <<<EOT
-        {
-            "id": 6361,
-            "post": 1384,
-            "parent": 0,
-            "author": 13253363,
-            "author_name": "tradesouthwest",
-            "author_url": "",
-            "date": "2023-02-22T17:46:29",
-            "date_gmt": "2023-02-22T17:46:29",
-            "content": {
-              "rendered": "<p>If you are working with custom post types or you are just not sure if the file you are working in has any direct access to the post, you can try this handy statement to get the ID of a post; outside of the loop even.</p>\n<pre class=\"wp-block-code\"><code lang=\"php\" class=\"language-php \">global $post;  \r\n    $post_id = ( empty( $post-&gt;ID ) ) ? get_the_ID() : $post-&gt;ID;</code></pre>\n<p>Then use $post_id string to assure you have the post ID. Example usage:</p>\n<pre class=\"wp-block-code\"><code lang=\"php\" class=\"language-php \">if ( get_post_type( $post_id ) != 'wpbdp_listing' ) return;</code></pre>\n"
-            },
-            "link": "https://developer.wordpress.org/reference/functions/get_the_id/#comment-6361",
-            "status": "approved",
-            "type": "comment",
-            "author_avatar_urls": {
-              "24": "https://secure.gravatar.com/avatar/73447974bbce11c37a245aac45e0cb3b?s=24&d=mm&r=g",
-              "48": "https://secure.gravatar.com/avatar/73447974bbce11c37a245aac45e0cb3b?s=48&d=mm&r=g",
-              "96": "https://secure.gravatar.com/avatar/73447974bbce11c37a245aac45e0cb3b?s=96&d=mm&r=g"
-            },
-            "meta": [
-              
-            ],
-            "_links": {
-              "self": [
-                {
-                  "href": "https://developer.wordpress.org/wp-json/wp/v2/comments/6361"
-                }
-              ],
-              "collection": [
-                {
-                  "href": "https://developer.wordpress.org/wp-json/wp/v2/comments"
-                }
-              ],
-              "author": [
-                {
-                  "embeddable": true,
-                  "href": "https://developer.wordpress.org/wp-json/wp/v2/users/13253363"
-                }
-              ],
-              "up": [
-                {
-                  "embeddable": true,
-                  "post_type": "wp-parser-function",
-                  "href": "https://developer.wordpress.org/wp-json/wp/v2/wp-parser-function/1384"
-                }
-              ]
-            }
-          }
-EOT;
-        
-        $item = json_decode( $item );
+        // Item as object
+        $item = new stdClass;
+        $item->id = 6361;
+        $item->post = 1384;
+        $item->parent = 0;
+        $item->author = 13253363;
+        $item->author_name = 'tradesouthwest';
+        $item->author_url = '';
+        $item->date = '2023-02-22T17:46:29';
+        $item->date_gmt = '2023-02-22T17:46:29';
+        $item->content = new stdClass;
+        $item->content->rendered = '<p>If you are working with custom post types or you are just not sure if the file you are working in has any direct access to the post, you can try this handy statement to get the ID of a post; outside of the loop even.</p>\n<pre class="wp-block-code"><code lang="php" class="language-php ">global $post;  \r\n    $post_id = ( empty( $post->ID ) ) ? get_the_ID() : $post->ID;</code></pre>\n<p>Then use $post_id string to assure you have the post ID. Example usage:</p>\n<pre class="wp-block-code"><code lang="php" class="language-php ">if ( get_post_type( $post_id ) != \'wpbdp_listing\' ) return;</code></pre>\n';
+        $item->link = 'https://developer.wordpress.org/reference/functions/get_the_id/#comment-6361';
+        $item->status = 'approved';
+        $item->type = 'comment';
+        $item->author_avatar_urls = array(
+            '24' => 'https://secure.gravatar.com/avatar/73447974bbce11c37a245aac45e0cb3b?s=24&d=mm&r=g',
+            '48' => 'https://secure.gravatar.com/avatar/73447974bbce11c37a245aac45e0cb3b?s=48&d=mm&r=g',
+            '96' => 'https://secure.gravatar.com/avatar/73447974bbce11c37a245aac45e0cb3b?s=96&d=mm&r=g'
+        );
+        $item->meta = array();
+        $item->_links = array(
+            'self' => array(
+                array(
+                    'href' => 'https://developer.wordpress.org/wp-json/wp/v2/comments/6361'
+                )
+            ),
+            'collection' => array(
+                array(
+                    'href' => 'https://developer.wordpress.org/wp-json/wp/v2/comments'
+                )
+            ),
+            'author' => array(
+                array(
+                    'embeddable' => true,
+                    'href' => 'https://developer.wordpress.org/wp-json/wp/v2/users/13253363'
+                )
+            ),
+            'up' => array(
+                array(
+                    'embeddable' => true,
+                    'post_type' => 'wp-parser-function',
+                    'href' => 'https://developer.wordpress.org/wp-json/wp/v2/wp-parser-function/1384'
+                )
+            )
+        );
 
         $snippet = $parser->parse_snippet( $item );
         $this->assertInstanceOf( Snippet::class, $snippet );
@@ -72,8 +65,9 @@ EOT;
         $now = date( 'Y-m-d H:i:s' );
 
         $this->assertEquals( 'bcd0083e747c5cbf955af85fb339232247416a2c9d1dd952e28a2814f7455bec', $snippet->get_id() );
-        $this->assertEquals( 'if ( get_post_type( $post_id ) != \'wpbdp_listing\' ) return;', $snippet->get_snippet() );
-        $this->assertEquals( '<p>If you are working with custom post types or you are just not sure if the file you are working in has any direct access to the post, you can try this handy statement to get the ID of a post; outside of the loop even.</p>\n<pre class=\"wp-block-code\"><code lang=\"php\" class=\"language-php \">global $post;  \r\n    $post_id = ( empty( $post-&gt;ID ) ) ? get_the_ID() : $post-&gt;ID;</code></pre>\n<p>Then use $post_id string to assure you have the post ID. Example usage:</p>\n<pre class=\"wp-block-code\"><code lang=\"php\" class=\"language-php \">if ( get_post_type( $post_id ) != \'wpbdp_listing\' ) return;</code></pre>\n', $snippet->get_context() );
+        $this->assertEquals( 'php', $snippet->get_snippet()[0]['language'] );
+        $this->assertEquals( 'global $post;  \r\n    $post_id = ( empty( $post->ID ) ) ? get_the_ID() : $post->ID;', $snippet->get_snippet()[0]['code'] );
+        $this->assertEquals( '<p>If you are working with custom post types or you are just not sure if the file you are working in has any direct access to the post, you can try this handy statement to get the ID of a post; outside of the loop even.</p>\n<pre class="wp-block-code"><code lang="php" class="language-php ">global $post;  \r\n    $post_id = ( empty( $post->ID ) ) ? get_the_ID() : $post->ID;</code></pre>\n<p>Then use $post_id string to assure you have the post ID. Example usage:</p>\n<pre class="wp-block-code"><code lang="php" class="language-php ">if ( get_post_type( $post_id ) != \'wpbdp_listing\' ) return;</code></pre>\n', $snippet->get_context() );
         $this->assertEquals( 'reference', $snippet->get_source() );
         $this->assertEquals( 'https://developer.wordpress.org/reference/functions/get_the_id/#comment-6361', $snippet->get_url() );
         $this->assertEquals( array( 'WordPress' ), $snippet->get_tags() );
